8e6b8e17e14f6ff7cb81816e90fafedac25ab40e
eleshinlomo <seun.olatunji2@gmail.com>
2023-03-06 15:36:35 -0500

    register page updated

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,57 +4,71 @@ import '../styles/Home.css'
 import { useEffect, useState } from 'react';
 
 const Register = () => {
-    const [username, setUsername] = useState([])
-    const [firstname, setFirstname] = useState([])
-    const [lastname, setLastname] = useState([])
-    const [email, setEmail] = useState([])
-    const [password, setPassword] = useState([])
+
+    const [firstname, setFirstname] = useState("")
+    const [lastname, setLastname] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [message, setMessage] = useState("This is message")
 
     const submitHandler =(e)=>{
         e.preventDefault()
-        fetch('', {
+        fetch('http://localhost:3500/api/addUser', {
             method: "POST",
             mode: "cors",
-            headers: {"Content-Type": "application/json"},
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
             body: JSON.stringify({
                 email: email,
                 firstname: firstname,
                 lastname: lastname,
-                username: username,
                 password: password
-
             })
         })
 
+        .then((result)=>{
+            if(!result){
+                setMessage("There seem to be an error. Please check your responses.")
+            }else{
+                setMessage(`Hello ${firstname}, you are now registered. 
+                We are happy to see how this CRM turns your business around.`)
+                console.log(result)
+                return result
+            }
+        })
+        .catch((err)=>console.log(err))
         
     }
 
     
     
   return (
-    <div className='contianer'>
+    <div className='register-container'>
     <main>
         <div className='image-holder'>
             <img src="https://images.unsplash.com/photo-1573167627769-e201a7ddf409?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="advert"/>
         </div>
 
        <div className='form-content-register'>
+        <div className='message'>
+        {message}
+        </div>
         <form onSubmit={submitHandler}>
             <label for="email">Email</label>
-            <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} placeholder='email' />
+            <input type="email" onChange={(e)=>setEmail(e.target.value)} name="email" value={email} placeholder='email' required />
             <label for="firstname">Firstname</label>
-            <input type="text" onChange={(e)=>setFirstname(e.target.value)} value={firstname} placeholder='firstname'  />
+            <input type="text" onChange={(e)=>setFirstname(e.target.value)} name="firstname" value={firstname} placeholder='firstname' required />
             <label for="lastname">Lastname</label>
-            <input type="text" onChange={(e)=>setLastname(e.target.value)} value={lastname} placeholder='lastname' />
-            <label for="username">Username</label>
-            <input type="username" onChange={(e)=>setUsername(e.target.value)} value={username} placeholder='username'  />
+            <input type="text" onChange={(e)=>setLastname(e.target.value)} name="lastname" value={lastname} placeholder='lastname' required />
             <label for="password">Password</label>
-            <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} placeholder='password' />
+            <input type="password" onChange={(e)=>setPassword(e.target.value)} name="password" value={password} placeholder='password' required />
             <button type='submit' className='register-btn'>REGISTER</button>
            </form>
            <Link to="/" className='home-btn2'><button>HOME</button></Link>
            </div>
-           {email}, {password}, {firstname}, {username}, {lastname}
+           
     </main>
     </div>
   )
